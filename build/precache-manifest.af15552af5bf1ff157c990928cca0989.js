b7fa3b619d32af80eae7cc97301561ab376374ae
StephonAutery <57854409+StephonAutery@users.noreply.github.com>
2021-05-14 18:57:16 -0700

    build

---
+++ build/precache-manifest.af15552af5bf1ff157c990928cca0989.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b7b3a096ea06270becc",
+    "url": "/StephonAutery/sautery-react-portfolio/static/css/main.271ec2f0.chunk.css"
+  },
+  {
+    "revision": "9b7b3a096ea06270becc",
+    "url": "/StephonAutery/sautery-react-portfolio/static/js/main.71f9e2c8.chunk.js"
+  },
+  {
+    "revision": "8e12b42533a68d4a75a7",
+    "url": "/StephonAutery/sautery-react-portfolio/static/js/runtime~main.d0e53749.js"
+  },
+  {
+    "revision": "3c57795873c71b73a233",
+    "url": "/StephonAutery/sautery-react-portfolio/static/js/2.0850b13b.chunk.js"
+  },
+  {
+    "revision": "57daf9da64fa6226f7598b0f253a15ba",
+    "url": "/StephonAutery/sautery-react-portfolio/index.html"
+  }
+];
